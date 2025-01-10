34d531066f2d51904f7d747ff1c1d6d63a4ceb02
haoziqaq <357229046@qq.com>
2025-01-11 00:23:00 +0800

    test: update test snaps

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,86 +3,19 @@ import { defineConfig } from '../dist/index.js'
 
 describe('test config', () => {
   it('option is undefined', () => {
-    expect(defineConfig()).toStrictEqual({
-      printWidth: 120,
-      singleQuote: true,
-      semi: false,
-      importOrder: ['<BUILTIN_MODULES>', '^vue$', '^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^~/(.*)$', '^[.]'],
-      plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss', 'prettier-plugin-packagejson'],
-      overrides: [
-        {
-          files: '**/package.json',
-          options: {
-            packageSortOrder: [],
-          },
-        },
-      ],
-    })
+    expect(defineConfig()).toMatchSnapshot()
   })
 
   it('sortImports is false', () => {
-    expect(defineConfig({ sortImports: false })).toStrictEqual({
-      printWidth: 120,
-      singleQuote: true,
-      semi: false,
-      importOrder: undefined,
-      plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-packagejson'],
-      overrides: [
-        {
-          files: '**/package.json',
-          options: {
-            packageSortOrder: [],
-          },
-        },
-      ],
-    })
+    expect(defineConfig({ sortImports: false })).toMatchSnapshot()
   })
 
   it('sortAtomicClass is false', () => {
-    expect(defineConfig({ sortAtomicClass: false })).toStrictEqual({
-      printWidth: 120,
-      singleQuote: true,
-      semi: false,
-      importOrder: ['<BUILTIN_MODULES>', '^vue$', '^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^~/(.*)$', '^[.]'],
-      plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-packagejson'],
-      overrides: [
-        {
-          files: '**/package.json',
-          options: {
-            packageSortOrder: [],
-          },
-        },
-      ],
-    })
+    expect(defineConfig({ sortAtomicClass: false })).toMatchSnapshot()
   })
 
   it('sortPackageJson is false', () => {
-    expect(defineConfig({ sortPackageJson: false })).toStrictEqual({
-      printWidth: 120,
-      singleQuote: true,
-      semi: false,
-      importOrder: ['<BUILTIN_MODULES>', '^vue$', '^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^~/(.*)$', '^[.]'],
-      plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
-      overrides: [],
-    })
-  })
-
-  it('custom packageSortOrder', () => {
-    expect(defineConfig({ packageSortOrder: ['name', 'version', 'description', 'scripts'] })).toStrictEqual({
-      printWidth: 120,
-      singleQuote: true,
-      semi: false,
-      importOrder: ['<BUILTIN_MODULES>', '^vue$', '^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^~/(.*)$', '^[.]'],
-      plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss', 'prettier-plugin-packagejson'],
-      overrides: [
-        {
-          files: '**/package.json',
-          options: {
-            packageSortOrder: ['name', 'version', 'description', 'scripts'],
-          },
-        },
-      ],
-    })
+    expect(defineConfig({ sortPackageJson: false })).toMatchSnapshot()
   })
 
   it('custom config', () => {
@@ -100,32 +33,6 @@ describe('test config', () => {
           },
         ],
       }),
-    ).toStrictEqual({
-      printWidth: 120,
-      singleQuote: true,
-      semi: true,
-      importOrder: ['<BUILTIN_MODULES>', '^vue$', '^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^~/(.*)$', '^[.]'],
-      plugins: [
-        '@ianvs/prettier-plugin-sort-imports',
-        'prettier-plugin-tailwindcss',
-        'prettier-plugin-packagejson',
-        'prettier-plugin-test',
-      ],
-      overrides: [
-        {
-          files: '**/package.json',
-          options: {
-            packageSortOrder: [],
-          },
-        },
-        {
-          files: '*.md',
-          options: {
-            organizeImportsSkipDestructiveCodeActions: true,
-          },
-        },
-      ],
-      vueIndentScriptAndStyle: true,
-    })
+    ).toMatchSnapshot()
   })
 })
